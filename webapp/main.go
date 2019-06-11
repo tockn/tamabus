e342056e6d95cf6f6c284b76a8aa430415229450
tockn <s.taku.inthebox@gmail.com>
2019-06-11 18:10:08 +0900

    up

--- webapp/main.go
+++ webapp/main.go
@@ -10,14 +10,16 @@ import (
 func main() {
 	var port string
 	var env string
+	var dbconfigPath string
 
 	flag.StringVar(&port, "port", "8080", "listening port.")
 	flag.StringVar(&env, "environment", "development", "environment")
+	flag.StringVar(&dbconfigPath, "dbconfig", "../dbconfig.yml", "dbconfig")
 
 	flag.Parse()
 
 	s := server.NewServer()
-	if err := s.Setup("dbconfig.yml", env); err != nil {
+	if err := s.Setup(dbconfigPath, env); err != nil {
 		log.Fatalf("server setup error. %s", err)
 	}
 	if err := s.Run(port); err != nil {
